09f35fe61a58cf5e099c8858696fcdec3f580a44
albipa <albion@paulsson.net>
2022-04-06 15:04:36 +0200

    continued on tkinter

--- src/main.py
+++ src/main.py
@@ -1,21 +1,18 @@
 import tkinter as tk
-import src.colors as color
-tilesx = 0
-tilesy = 0
-
+import colors as color
+tilesx = 600
+tilesy = 600
+#w, h
 class Game(tk.Frame):
-    def __init__(self, w, h):
+    def __init__(self):
         tk.Frame.__init__(self)
         self.grid()
         self.master.title("4096")
-
-        self.main_grid = tk.Frame(
-            self, bg=color.GRID_COLOR, bd=3, width=w, height=h
-        )
+        self.main_grid = tk.Frame(self, bg=color.GRID_COLOR, bd=3, width=600, height=600)
         self.main_grid.grid(pady=(100,0))
 
     def gui(self):
-        self.cells = []
+        self.tiles = []
         for i in range(4):
             row = []
             for j in range(4):
@@ -24,12 +21,16 @@ class Game(tk.Frame):
                 )
                 tile_frame.grid(row=i, column=j, padx=5, pady=5)
                 tile_number = tk.Label(self.main_grid, bg=color.EMPTY_CELL_COLOR)
+                tile_number.grid(row=i, column=j)
+                tile_data = {"frame": cell_frame, "number": cell_number}
+                row.append(tile_data)
+            self.tiles.append(row)
 
 
 def main():
-    x = input("Size X: ")
-    y = input("Size Y: ")
-    Game(int(x), int(y))
+    #x = input("Size X: ")
+    #y = input("Size Y: ")int(x), int(y)
+    Game()
 
 if __name__ == "__main__":
     main()
